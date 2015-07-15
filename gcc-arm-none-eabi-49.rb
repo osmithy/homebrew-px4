dbeebf7a4558b56438ab791456a0f35811af2e58
osmithy <owen@othermachine.co>
2015-07-15 13:18:15 -0700

    fixed download link

--- gcc-arm-none-eabi-49.rb
+++ gcc-arm-none-eabi-49.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GccArmNoneEabi49 < Formula
   homepage 'https://launchpad.net/gcc-arm-embedded'
-  version '20150623'
-  url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q1-update/+download/gcc-arm-none-eabi-4_9-2015q2-20150609-mac.tar.bz22015-06-23'
+  version '20150609'
+  url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q2-update/+download/gcc-arm-none-eabi-4_9-2015q2-20150609-mac.tar.bz2'
   md5 '34904f10367d622c139c782063212cd9'
 
   def install 
