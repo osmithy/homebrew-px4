94609730e053dff85fcff8d5c9f99595547c5cb5
osmithy <owen@othermachine.co>
2015-07-15 13:25:18 -0700

    fixed sha1

--- gcc-arm-none-eabi-49.rb
+++ gcc-arm-none-eabi-49.rb
@@ -4,7 +4,7 @@ class GccArmNoneEabi49 < Formula
   homepage 'https://launchpad.net/gcc-arm-embedded'
   version '20150609'
   url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q2-update/+download/gcc-arm-none-eabi-4_9-2015q2-20150609-mac.tar.bz2'
-  sha1 'bfb3547598503aa74755680a93cdbe99'
+  sha1 '7f2d8c3644cd76da1b78d17a0770c8c0efbff957'
 
   def install 
     ohai 'Copying binaries...'
